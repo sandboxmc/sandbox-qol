4854ba160ce202faeef03f25901bb6175a7c60b9
Tyler Crocker <crocker.tylerj@gmail.com>
2022-09-27 18:35:07 -0700

    Reworking a bunch of stuff to have this take over as a more thorough repo

--- src/main/java/io/sandbox/qol/Main.java
+++ src/main/java/io/sandbox/qol/Main.java
@@ -1,10 +1,13 @@
-package io.sandbox.rails;
+package io.sandbox.qol;
 
 import net.fabricmc.api.ModInitializer;
+
+import io.sandbox.lib.Config;
 
 public class Main implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		System.out.println("Initializing Sandbox Speed Rails");
+		Config<Object> a = new Config<Object>(Object.class, "my-config.json");
 	}
 }
